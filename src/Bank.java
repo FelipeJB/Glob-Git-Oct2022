13f48a3548c70f7f376cf4bc0348dacb6d8c67ad
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-10-14 21:39:13 -0500

    Class Bank created and run test done successfully.

---
+++ src/Bank.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Bank {
+
+    public String name;
+
+    Bank(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return this.name; }
+
+    Client Ana = new Client(123, "Ana", "dad$%&$");
+
+}
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import jdk.internal.org.objectweb.asm.tree.analysis.Analyzer;
 
 public class Main {
     public static void main(String[] args) {
-        Client Ana = new Client(189, "Ana", "das3654" );
-        System.out.println(Ana.myBankAccount.getOpeningDate());
+        Bank myBank = new Bank("Morgan Stanley");
+        System.out.println("Welcome to " + myBank.getName());
+        System.out.println(myBank.Ana.myBankAccount.getOpeningDate());
     }
 }
