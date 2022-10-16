10498ba3a4e91a4d7f33c557e10e2ece39012017
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-10-15 21:15:50 -0500

    Option panel and withdraw method fixed

--- src/Main.java
+++ src/Main.java
@@ -28,43 +28,47 @@ public class Main {
         int option;
         do {
             System.out.println("Select your option");
-            System.out.println("    1. Deposit");
-            System.out.println("    2. Withdraw");
-            System.out.println("    3. Transfer");
-            System.out.println("    4. Exit");
+            System.out.println("    1. Current balance");
+            System.out.println("    2. Deposit");
+            System.out.println("    3. Withdraw");
+            System.out.println("    4. Transfer");
+            System.out.println("    5. Exit");
             option = myScanner.nextInt();
             myScanner = new Scanner(System.in);
-            if (option < 1 || option > 4) {
+            if (option < 1 || option > 5) {
                 System.out.println("Invalid option. Insert a number between 1 and 3\n");
-            } else if (option >= 1 && option <= 4) {
+            } else if (option >= 1 && option <= 5) {
                 switch (option) {
                     case 1:
+                        System.out.println("Your current balance is USD " + MorganStanley.Ana.myBankAccount.getBalance() + ".\n");
+                        break;
+                    case 2:
                         System.out.println("Insert deposit amount (USD)");
                         float moneyAmount = myScanner.nextFloat();
                         myScanner = new Scanner(System.in);
                         MorganStanley.Ana.myBankAccount.depositMoney(moneyAmount);
                         System.out.println("Current Balance: USD " + MorganStanley.Ana.myBankAccount.getBalance() + ".\n");
                         break;
-                    case 2:
+                    case 3:
                         System.out.println("Insert withdraw amount (USD)");
                         float moneyAmountW = myScanner.nextFloat();
                         myScanner = new Scanner(System.in);
-                        MorganStanley.Ana.myBankAccount.withdrawMoney(moneyAmountW);
                         if (moneyAmountW > MorganStanley.Ana.myBankAccount.balance){
                             System.out.println("Withdraw amount cannot be higher than balance account.\n");
                             System.out.println("Current Balance: USD " + MorganStanley.Ana.myBankAccount.getBalance() + ".\n");
                         } else {
+                            MorganStanley.Ana.myBankAccount.withdrawMoney(moneyAmountW);
                             System.out.println("Current Balance: USD " + MorganStanley.Ana.myBankAccount.getBalance() + ".\n");
                         }
                         break;
-                    case 3:
+                    case 4:
                         System.out.println("Testing Transfer!");
                         break;
-                    case 4:
+                    case 5:
                         System.out.println("Have a great day!");
                         break;
                 }
             }
-        } while (option !=4 );
+        } while (option !=5 );
     }
 }
